da0deb2dabea5b3a858a6e97a7d362d5075d176c
ssenabulya <ssenabulyawilsonm@gmail.com>
2014-06-27 10:32:17 +0300

    my ruby apps

---
+++ Case.rb
@@ -0,0 +1,8 @@
+puts "enter a number"
+number= gets.to_i
+case 
+when (number % 2 == 0)
+puts" the number is : even"
+else
+puts " the number is : odd"
+end
---
+++ implement.rb
@@ -0,0 +1,8 @@
+puts "enter a number"
+number = gets.to_i
+case
+when (number >= 5 && number <= 10)
+puts "number in range"
+ else
+puts "number not in range"
+end
